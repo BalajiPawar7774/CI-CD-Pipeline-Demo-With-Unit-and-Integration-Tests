8eb46a9f84f46ca0947561e46fa864eea4df592e
Balaji pawar <balajipawar9259@gmail.com>
2025-11-26 11:25:53 +0530

    Created Model, ApplicationDbContext, ConnectionString,

---
+++ BookApi/Dal/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApi.Dal
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
